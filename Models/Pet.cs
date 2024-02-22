4d70246463806c0c460014578220af422adc60fa
Gavin Popken <gavinpopkenart@gmail.com>
2024-02-22 13:57:33 -0600

    finished the model for Pets and updated db

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -3,9 +3,44 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System;
 
-namespace Pet_Hotel
+namespace Pet_Hotel.Models
 {
-    public enum PetBreedType {}
-    public enum PetColorType {}
-    public class Pet {}
+    public enum PetBreedType 
+    {
+        Sherpherd,
+        Poodle,
+        Beagle,
+        Bulldog,
+        Terrier,
+        Boxer,
+        Labrador,
+        Retriever
+    }
+    public enum PetColorType 
+    {
+        White,
+        Black,
+        Golden,
+        Tricolor,
+        Spotted
+    }
+
+    public class Pet 
+    {
+        public int id {get; set;}
+        [Required]
+        public string name {get; set;}
+
+        [Required]
+        public string petBreed {get; set;}
+
+        [Required]
+        public string petColor {get; set;}
+
+        public DateTime checkedInAt {get; set;}
+
+        [ForeignKey("petOwner")]
+        public int petOwnerId {get; set;}
+        public PetOwner petOwner;
+    }
 }
